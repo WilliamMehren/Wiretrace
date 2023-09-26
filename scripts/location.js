f236b3f033f7e229a4b27874c07047986db374a2
Filip Melheim <filipmelh@viken.no>
2023-09-26 15:13:40 +0200

    Commented code in location.js

--- scripts/location.js
+++ scripts/location.js
@@ -1,12 +1,23 @@
+// Define a function to get the city and country from an IP address
 function getCityCountryFromIp(ip) {
+    // Create a new XMLHttpRequest object
     var request = new XMLHttpRequest();
+    
+    // Open a GET request to the API endpoint with the provided IP address
     request.open('GET', 'https://ipapi.co/' + ip + '/json/', false);  // false for synchronous request
+    
+    // Send the request
     request.send();
     
+    // Check if the request was successful (status code 200)
     if (request.status === 200) {
-      var response = JSON.parse(request.responseText);
-      return response.city + ', ' + response.country_name;
+        // Parse the response as JSON
+        var response = JSON.parse(request.responseText);
+        
+        // Return the city and country name from the response
+        return response.city + ', ' + response.country_name;
     } else {
-      return 'Drammen , Norway';
+        // Return a default location string if the request fails
+        return 'Drammen , Norway';
     }
-  }
+}
