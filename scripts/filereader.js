537275f61e8ebd3d23aebb04053db5633c81126a
willi <willi@DERECTORYDESTRO>
2023-09-29 12:20:21 +0200

    start of connection

--- scripts/filereader.js
+++ scripts/filereader.js
@@ -1,10 +1,19 @@
 var filedata
 var isLoaded = false
-function getFile(input){
+async function getFile(input){
     var reader = new FileReader();
     reader.addEventListener('load', (event) => {
         filedata = JSON.parse(reader.result);
         isLoaded = true
+        var html = ""
+        console.log(filedata)
+        filedata["ips"].forEach(ip => {
+        var place =  getCityCountryFromIp(ip)
+        var coords = getLocationFromIp(ip)
+        html += `<tr><td>${ip}</td><td>${place}</td><td><button onclick="map.setView(${coords})">${coords}</button></td></tr>`
+        });
+        document.getElementById("data").innerHTML = "<tr><th>IP</th><th>location</th><th>coordinates</th></tr>"+ html
       });
-      reader.readAsText(input.files[0]);
-}
+      filedata = reader.readAsText(input.files[0]);
+      
+    }
