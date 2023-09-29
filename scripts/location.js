03c06d36962865bd2cc9c5dbfcd26a5af19f0e1b
willi <willi@DERECTORYDESTRO>
2023-09-29 08:26:40 +0200

    new location script

--- scripts/location.js
+++ scripts/location.js
@@ -2,22 +2,38 @@
 function getCityCountryFromIp(ip) {
     // Create a new XMLHttpRequest object
     var request = new XMLHttpRequest();
-    
     // Open a GET request to the API endpoint with the provided IP address
     request.open('GET', 'https://ipapi.co/' + ip + '/json/', false);  // false for synchronous request
-    
     // Send the request
     request.send();
-    
     // Check if the request was successful (status code 200)
     if (request.status === 200) {
         // Parse the response as JSON
         var response = JSON.parse(request.responseText);
-        
         // Return the city and country name from the response
-        return response.city + ', ' + response.country_name;
+        return response.city + ', ' + response.country;
     } else {
         // Return a default location string if the request fails
         return 'Drammen , Norway';
+    }
+}
+
+// Define a function to get the city and country from an IP address
+function getLocationFromIp(ip) {
+    // Create a new XMLHttpRequest object
+    var request = new XMLHttpRequest();
+    // Open a GET request to the API endpoint with the provided IP address
+    request.open('GET', 'https://ipapi.co/' + ip + '/json/', false);  // false for synchronous request
+    // Send the request
+    request.send();
+    // Check if the request was successful (status code 200)
+    if (request.status === 200) {
+        // Parse the response as JSON
+        var response = JSON.parse(request.responseText);
+        // Return the city and country name from the response
+        return response.latitude + ', ' + response.longitude;
+    } else {
+        // Return a default location string if the request fails
+        return '59.911491 , 10.757933';
     }
 }
